53912ce6746fb776eca59a5f3c56148a5233036d
jiangliang <jiangliang.net@gmail.com>
2018-12-06 14:12:33 +0800

    更新丢失模块

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <aliossflutter/AliossflutterPlugin.h>
+#import <image_picker/ImagePickerPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AliossflutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"AliossflutterPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+}
+
+@end
