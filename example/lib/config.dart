2528925f00102efc6b501ccb52e020e851371891
jiangliang <jiangliang.net@gmail.com>
2019-01-12 10:44:40 +0800

    配置文件

---
+++ example/lib/config.dart
@@ -0,0 +1,7 @@
+class Config{
+ static final  String stsserver="";
+ static final String endpoint="";
+ static final String cryptkey="";
+ static final String bucket="";
+ static final String key="";
+}
